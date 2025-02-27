a9623991e30b794552ba59d2255eafbb60ab69c6
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-02-26 22:17:29 -0600

    first commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header id="main-header" class="container">
+        <section class="section header-content"></section><!-- .header-content -->
+    </header><!-- #main-header -->
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Index Template
+ * 
+ * @package Stories
+ * @since 1.0.0
+ */
+get_header(); ?>
+
+<main id="main" class="site-main">
+    <div class="container">
+        <section class="section index">
+            <h1 class="title-page"><?php echo esc_html__( 'Index', 'stories' ); ?></h1>
+        </section>
+    </div>
+</main><!-- #main -->
+
+<?php get_footer(); ?>
