e163d55adf3b35c8d12b9fbeb5fd48ecbbd32a66
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-02-26 22:34:58 -0600

    show the loop

---
+++ template-parts/content-archive.php
@@ -0,0 +1,21 @@
+<article class="archive-post">
+    <header class="archive-post--header">
+        <?php
+            the_category();
+            if ( ! has_post_thumbnail() == false ) {
+                echo '<img class="thumbnail" src="'; the_post_thumbnail_url( 'media' ); echo '" alt="Picture post" loading="lazy" width="300" height="200">';
+            }
+        ?>
+    </header><!-- .archive-post--header -->
+    <div class="archive-post--content">
+        <a href="<?php the_permalink(); ?>" class="archive-post--permalink">
+            <?php the_title( '<h3 class="archive-post--title">', '</h3>' ); ?>
+        </a>
+        <?php
+            the_excerpt();
+            if ( ! is_active_sidebar( 'posts-sidebar' ) ) {
+                echo '<div class="tags">' . get_the_tag_list() . '</div>';
+            }
+        ?>
+    </div><!-- .archive-post--content -->
+</article><!-- .archive-post -->
