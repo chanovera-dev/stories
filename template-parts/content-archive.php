f396cf182534af962f4d6ae63ded708c54be1f65
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-02-27 11:54:49 -0600

    templates for content-archive

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -13,7 +13,7 @@
         </a>
         <?php
             the_excerpt();
-            if ( ! is_active_sidebar( 'posts-sidebar' ) ) {
+            if ( ! is_active_sidebar( 'sidebar-1' ) ) {
                 echo '<div class="tags">' . get_the_tag_list() . '</div>';
             }
         ?>
