f55459e2d0f7440129362767d1b85766be8c5b4d
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-02-28 14:54:41 -0600

    fix forbidde aria attributes

--- templates/header/menu-mobile-button.php
+++ templates/header/menu-mobile-button.php
@@ -1,4 +1,4 @@
-<div id="menu-mobile__button" onclick="openMenuMobile()" aria-label="Open menu mobile">
+<div id="menu-mobile__button" onclick="openMenuMobile()">
     <span class="bar"></span>
     <span class="bar"></span>
     <span class="bar"></span>
