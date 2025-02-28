2fec995507b1a6ea4f4a8c1a751e061de4f49d94
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-02-28 12:40:44 -0600

    header support - mobile and desktop

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,5 +1,5 @@
 function openMenuMobile() {
-    if ( window.innerWidth <= 1023 ) {
+    if ( window.innerWidth <= 767 ) {
         const btnMenuMobile = document.querySelector( '#menu-mobile__button' );
         const menuMobile = document.querySelector( '#primary' );
 
@@ -14,11 +14,11 @@ function openCustomSearchform() {
     const iconSearchBtn = document.querySelector( '#search-mobile__button .bi-search' );
     const iconCloseBtn = document.querySelector( '#search-mobile__button .bi-x-circle' );
 
-    if ( window.innerWidth <= 1023 ) {
+    if ( window.innerWidth <= 767 ) {
         customSearchform.classList.add( 'show' );
     }
 
-    if ( window.innerWidth >=1023 ) {
+    if ( window.innerWidth >=767 ) {
         customSearchform.classList.toggle( 'show' );
         primaryMenu.classList.toggle( 'hide' );
         iconSearchBtn.classList.toggle( 'hide' );
@@ -27,7 +27,7 @@ function openCustomSearchform() {
 }
 
 function closeCustomSearchform() {
-    if ( window.innerWidth <= 1023 ) {
+    if ( window.innerWidth <= 767 ) {
         const customSearchform = document.querySelector( '#custom-searchform' );
         customSearchform.classList.remove( 'show' );
     }
