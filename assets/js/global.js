0afaeb3979bcc9fd12293cf6377b26a327aa3169
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-02-28 14:43:54 -0600

    js for header - scroll actions

--- assets/js/global.js
+++ assets/js/global.js
@@ -32,3 +32,31 @@ function closeCustomSearchform() {
         customSearchform.classList.remove( 'show' );
     }
 }
+
+// add class for scroll
+function scrollActions() {
+    const body = document.body
+    const scrollUp = "scroll-up"
+    const scrollDown = "scroll-down"
+    let lastScroll = 0
+    
+    window.addEventListener("scroll", () => {
+        const currentScroll = window.pageYOffset
+        if (currentScroll <= 0) {
+            body.classList.remove(scrollUp)
+            return
+        }
+      
+      if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+        // down
+        body.classList.remove(scrollUp)
+        body.classList.add(scrollDown)
+      } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+        // up
+        body.classList.remove(scrollDown)
+        body.classList.add(scrollUp)
+      }
+      lastScroll = currentScroll
+    })
+}
+scrollActions();
