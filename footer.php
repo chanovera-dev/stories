8b04c7b3d1737ceedff30907bc126b52a077e666
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-03-07 00:03:38 -0600

    add a menu pages on footer

--- footer.php
+++ footer.php
@@ -1,4 +1,32 @@
         <footer id="main-footer">
+            <div class="container middle-footer">
+                <section class="section">
+                    <div class="other-links">
+                        <div class="group-links">
+                            <?php
+                                $menu_id_pages = get_nav_menu_locations()['pages'];
+                                $menu_pages = wp_get_nav_menu_object($menu_id_pages);
+                                $items_pages = wp_get_nav_menu_items($menu_id_pages);
+
+                                if ( ! empty($items_pages) ) {
+                                    echo '
+                                    <div>
+                                        <h3 class="title-menu">' . $menu_pages->name . '</h3>';
+                                        wp_nav_menu(
+                                            array(
+                                                'container' => 'nav',
+                                                'container_class' => 'pages',
+                                                'theme_location' = 'pages',
+                                            )
+                                        );
+                                    echo '
+                                    </div>';
+                                }
+                            ?>
+                        </div>
+                    </div>
+                </section>
+            </div>
             <div class="container end-footer">
                 <section class="section">
                     <p>© <?php bloginfo( 'name' ); echo ' ' . date("Y"); ?> • <?= __('Todos los Derechos Reservados', 'stories') ?></p>
