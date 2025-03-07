255e1f3b42203acdd354a47eda847776f6559ef6
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-03-06 23:11:25 -0600

    end part for footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
         <footer id="main-footer">
             <div class="container end-footer">
                 <section class="section">
-                    <p>© <?php bloginfo( 'name' ); echo date("Y"); ?> • <?= __('Todos los Derechos Reservados', 'stories') ?></p>
+                    <p>© <?php bloginfo( 'name' ); echo ' ' . date("Y"); ?> • <?= __('Todos los Derechos Reservados', 'stories') ?></p>
                 </section>
             </div>
         </footer>
