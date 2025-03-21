4376bb374ec1e7fdd712e7b6510f71d53471ff42
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-03-21 10:32:31 -0600

    conditional for categories list on archive

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -16,22 +16,7 @@
         </a>
         <?php
             the_excerpt();
-
-            $sidebars_widgets = wp_get_sidebars_widgets();
-            $tag_cloud_active = false;
-
-            if ( ! empty( $sidebars_widgets['sidebar-posts'] ) ) {
-                foreach ( $sidebars_widgets['sidebar-posts'] as $widget_id ) {
-                    if ( strpos( $widget_id, 'tag_cloud' ) === 0 ) { 
-                        $tag_cloud_active = true;
-                        break;
-                    }
-                }
-            }
-
-            if ( ! $tag_cloud_active ) {
-                echo '<div class="tags">' . get_the_tag_list() . '</div>';
-            }
+            echo '<div class="tags">' . get_the_tag_list() . '</div>';
         ?>
     </div><!-- .archive-post--content -->
 </article><!-- .archive-post -->
