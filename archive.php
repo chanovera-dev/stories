55e81522c3f6c9542516da4f5dd8233244b152af
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-03-21 12:35:51 -0600

    add the breadcrumbs for archive, separate the css in a alone file

--- archive.php
+++ archive.php
@@ -8,22 +8,26 @@
 get_header(); ?>
 
 <main id="main" class="site-main" role="main">
-    <div class="container">
-        <section class="section top">
-            <div class="breadcrumbs">
-                <?php
-                    if ( function_exists('wp_breadcrumbs') ) {
-                        wp_breadcrumbs( '<p id="breadcrumbs">','</p>' );
-                    }
-                ?>
-            </div>
-        </section>
-    </div>
-    <header class="container container--title">
-        <section class="section">
-            <?php the_archive_title( '<h1 class="main-title">', '</h1>' ); ?>
-        </section>
-    </header><!-- .container--title -->
+
+    <header class="container post-header">
+        <div class="container">
+            <section class="section top">
+                <div class="breadcrumbs">
+                    <?php
+                        if ( function_exists('wp_breadcrumbs') ) {
+                            wp_breadcrumbs( '<p id="breadcrumbs">','</p>' );
+                        }
+                    ?>
+                </div>
+            </section>
+        </div>
+        <div class="container container--title">
+            <section class="section">
+                <?php the_archive_title( '<h1 class="main-title">', '</h1>' ); ?>
+            </section>
+        </div><!-- .container--title -->
+    </header><!-- .post-header -->
+
     <div class="container container--posts">
         <section class="section">
             <div class="posts">
--- inc/templates.php
+++ inc/templates.php
@@ -21,9 +21,9 @@ function get_asset_version($file_path) {
     if ( is_home() or is_archive() or is_search() ) {
 
         wp_enqueue_style( 'posts', get_template_directory_uri() . '/assets/css/posts.css', array(), get_asset_version('/assets/css/posts.css'), 'all' );
+        wp_enqueue_style( 'breadcrumbs', get_template_directory_uri() . '/assets/css/breadcrumbs.css', array(), get_asset_version('/assets/css/breadcrumbs.css'), 'all' );
 
         if ( is_active_sidebar( 'sidebar-posts' ) ) {
-            wp_enqueue_style( 'sidebar', get_template_directory_uri() . '/assets/css/sidebar.css', array(), get_asset_version('/assets/css/posts.css'), 'all' );
             wp_enqueue_style( 'sidebar', get_template_directory_uri() . '/assets/css/sidebar.css', array(), get_asset_version('/assets/css/sidebar.css'), 'all' );
         }
     }
@@ -36,6 +36,7 @@ add_action( 'wp_enqueue_scripts', 'posts_styles' );
 function post_styles() {
     if ( is_single() or is_page() ) {
 
+        wp_enqueue_style( 'breadcrumbs', get_template_directory_uri() . '/assets/css/breadcrumbs.css', array(), get_asset_version('/assets/css/breadcrumbs.css'), 'all' );
         wp_enqueue_style( 'single', get_template_directory_uri() . '/assets/css/single.css', array(), get_asset_version('/assets/css/single.css'), 'all' );
         
         if ( has_post_thumbnail() == true ) {
