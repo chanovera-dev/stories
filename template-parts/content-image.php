92a2f0ad5a4e764e1a26a385b55f1c82e8188864
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-04-02 16:20:32 -0600

    image post

--- template-parts/content-image.php
+++ template-parts/content-image.php
@@ -1,3 +1,5 @@
 <article class="image-post">
-    <?php the_content(); ?>
+    <?php
+        if ( has_post_thumbnail() == true ) : echo '<img class="thumbnail" src="'; the_post_thumbnail_url( 'full' ); echo '" alt="Imagen del artículo" loading="lazy" width="300" height="200">'; endif;
+    ?>
 </article><!-- .image-post -->
