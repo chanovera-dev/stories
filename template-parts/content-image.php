472cff042a0f0eca0906f24dcf56319c11ee436e
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-04-02 16:28:15 -0600

    image post

--- template-parts/content-image.php
+++ template-parts/content-image.php
@@ -1,5 +1,17 @@
 <article class="image-post">
-    <?php
-        if ( has_post_thumbnail() == true ) : echo '<img class="thumbnail" src="'; the_post_thumbnail_url( 'full' ); echo '" alt="Imagen del artículo" loading="lazy" width="300" height="200">'; endif;
-    ?>
+    <header class="image-post--header">
+        <?php
+            the_category();
+            
+            if ( ! has_post_thumbnail() == false ) {
+                echo '<img class="thumbnail" src="'; the_post_thumbnail_url( 'media' ); echo '" alt="Picture post" loading="lazy" width="300" height="200">';
+            }
+        ?>
+    </header><!-- .image-post--header -->
+    <div class="image-post--content">
+        <?php
+            echo '<div class="date">' . get_the_date() . '</div>';
+            echo '<div class="tags">' . get_the_tag_list() . '</div>';
+        ?>
+    </div><!-- .image-post--content -->
 </article><!-- .image-post -->
