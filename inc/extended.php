d2dc5dd7ff7bb780f339aaddc40155c0c4599e64
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-04-08 13:32:54 -0600

    convert the loop for detras del espejo in a shortcode and add the short code to the detras del espejo page in the WordPress CMS

--- inc/extended.php
+++ inc/extended.php
@@ -229,3 +229,38 @@ function replace_menu_links_with_div($item_output, $item, $depth, $args) {
     return $item_output;
 }
 add_filter('walker_nav_menu_start_el', 'replace_menu_links_with_div', 10, 4);
+
+/**
+ * Create a shortcode for "detras-del-espejo" posts
+ */
+function shortcode_detras_del_espejo() {
+    ob_start(); // Iniciar el almacenamiento en búfer de salida
+
+    $args = array(
+        'post_type' => 'detras-del-espejo',
+        'post_status' => 'publish',
+        'paged' => get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1,
+    );
+    $query = new WP_Query( $args );
+
+    if ( $query->have_posts() ) {
+        while ( $query->have_posts() ) {
+            $query->the_post();
+            get_template_part( 'template-parts/content', 'archive-dde' );
+        }
+
+        the_posts_pagination( array(
+            'mid_size'  => 2,
+            'prev_text' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/></svg>Anterior',
+            'next_text' => 'Siguiente <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/></svg>'
+        ) );
+
+    } else {
+        echo '<p>' . esc_html__( 'No posts found', 'stories' ) . '</p>';
+    }
+    wp_reset_postdata();
+
+    return ob_get_clean(); // Devolver el contenido almacenado
+}
+
+add_shortcode('detras_del_espejo', 'shortcode_detras_del_espejo');
