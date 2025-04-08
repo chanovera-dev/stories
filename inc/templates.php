10406019e7793dc11a36cf2ea91440bdfd0a54a2
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-04-07 19:41:11 -0600

    sidebar access

--- inc/templates.php
+++ inc/templates.php
@@ -20,7 +20,8 @@ function posts_styles() {
         wp_enqueue_style( 'posts', get_template_directory_uri() . '/assets/css/posts.css', array(), get_asset_version('/assets/css/posts.css'), 'all' );
         wp_enqueue_style( 'breadcrumbs', get_template_directory_uri() . '/assets/css/breadcrumbs.css', array(), get_asset_version('/assets/css/breadcrumbs.css'), 'all' );
 
-        if ( is_active_sidebar( 'sidebar-posts' ) && ( is_home() or is_search() or ( is_archive() && get_post_type() === 'post' ) ) ) {
+        if (
+            is_active_sidebar( 'sidebar-posts' ) && ( is_home() || is_search() || ( is_archive() && ( ! is_post_type_archive() || is_post_type_archive( 'post' ) ) ) ) ) {
             wp_enqueue_style( 'sidebar', get_template_directory_uri() . '/assets/css/sidebar.css', array(), get_asset_version('/assets/css/sidebar.css'), 'all' );
         }
     }
