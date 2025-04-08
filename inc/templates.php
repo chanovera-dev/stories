c736dcb70430d4c55afdb6a4573d58ca49b77f3c
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-04-08 15:06:01 -0600

    error 404 styles

--- inc/templates.php
+++ inc/templates.php
@@ -71,7 +71,7 @@ add_action( 'wp_enqueue_scripts', 'post_styles', 20 );
  */
 function page404_styles() {
     if ( is_404() ) {
-        wp_enqueue_style( 'error404-styles', get_template_directory_uri() . '/assets/css/error404.css' );
+        wp_enqueue_style( 'error404-styles', get_template_directory_uri() . '/assets/css/error404.css', array(), get_asset_version('/assets/css/error404.css'), 'all' );
     }
 }
 add_action( 'wp_enqueue_scripts', 'page404_styles' );
