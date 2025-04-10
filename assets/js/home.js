25fa2d71e35b51b955744c608ebdce1f95f0e177
Cristian <cristian@MBP-de-Cristian.izzigo.tv>
2025-04-09 19:06:14 -0600

    text input effect in the startup header

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,36 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
     const titleEl = document.querySelector(".site-main .container--header .section .message article .main-title");
     const refEl = document.querySelector(".site-main .container--header .section .message article .reference");
-  
+
     if (titleEl && refEl) {
-      const titleText = titleEl.textContent.trim();
-      const refText = refEl.textContent.trim();
-  
-      titleEl.textContent = "";
-      refEl.textContent = "";
-  
-      function typeAnimated(el, text, callback) {
+    const titleText = titleEl.textContent.trim();
+    const refText = refEl.textContent.trim();
+
+    titleEl.textContent = "";
+    refEl.textContent = "";
+
+    function typeAnimated(el, text, callback) {
         let i = 0;
         function type() {
-          if (i < text.length) {
+        if (i < text.length) {
+            const char = text[i];
             const span = document.createElement("span");
             span.classList.add("letter");
-            span.style.animationDelay = `${i * 0.03}s`; // Pequeño delay para que no se encimen
-            span.textContent = text[i];
+
+            if (char === " ") {
+            span.classList.add("space");
+            span.innerHTML = "&nbsp;";
+            } else {
+            span.textContent = char;
+            span.style.animationDelay = `${i * 0.03}s`;
+            }
+
             el.appendChild(span);
             i++;
             setTimeout(type, 100); // velocidad por letra
-          } else if (typeof callback === "function") {
+        } else if (typeof callback === "function") {
             callback();
-          }
+        }
         }
         type();
-      }
-  
-      setTimeout(() => {
+    }
+
+    setTimeout(() => {
         typeAnimated(titleEl, titleText, () => {
-          typeAnimated(refEl, refText);
+        typeAnimated(refEl, refText);
         });
-      }, 1000); // espera 1 segundo antes de empezar
+    }, 1000);
     }
 });
+
