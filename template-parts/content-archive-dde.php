f9c50bc7da4ec5ca3b9312ce61970bbb91728a05
Chano Vera <github@chanovera.com>
2025-10-02 18:32:54 -0600

    update design width animations

--- template-parts/content-archive-dde.php
+++ template-parts/content-archive-dde.php
@@ -1,7 +1,7 @@
-<article class="archive-post">
-    <div class="archive-post--content">
-        <a href="<?php the_permalink(); ?>" class="archive-post--permalink">
-            <?php the_title( '<h3 class="archive-post--title">', '</h3>' ); ?>
+<article class="post">
+    <div class="post--content">
+        <a href="<?php the_permalink(); ?>" class="post--permalink">
+            <?php the_title( '<h3 class="post--title">', '</h3>' ); ?>
         </a>
         <?php the_excerpt(); ?>
     </div><!-- .archive-post--content -->
